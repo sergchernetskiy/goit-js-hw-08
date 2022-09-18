fde341b793ea7d2c620b8723e39faeec0a746e03
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-18 16:58:37 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+///////// Почати з пункту 4
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+  {
+    duration: 61.857;
+    percent: 0.049;
+    seconds: 3.034;
+  }
+};
+
+player.on('play', onPlay);
