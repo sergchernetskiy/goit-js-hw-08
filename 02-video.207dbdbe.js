5a957833612b1cdb40d31072e8f59631013d0a94
sergchernetskiy <104746735+sergchernetskiy@users.noreply.github.com>
2022-09-20 16:21:23 +0000

    Deploying to gh-pages from @ sergchernetskiy/goit-js-hw-08@cf7853003102540b8733a11993c14219db93b577 🚀

--- 02-video.207dbdbe.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));o.on("play",(function(e){}));
-//# sourceMappingURL=02-video.207dbdbe.js.map
--- 02-video.af955658.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));o.on("play",(function(e){}))}();
-//# sourceMappingURL=02-video.af955658.js.map
