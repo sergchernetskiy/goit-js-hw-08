f83f299d32df1bf7cff438959e99f97c817fc70d
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2022-09-21 15:07:01 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,28 @@
-// імпортуємо плеєр та пакет тротл бібліотеки лодаш
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// створюємо посилання, копію об'єкта плеєр, статичну змінну для ключа в LocalStorage
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
+let parsedTime;
 //console.log(player);
 
-// додаємо слухача подій з відстеженням в 1 секунду
-player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
 
-// колбек LocalStorage, що робить запис поточних параметрів об'єкта timeupdate
-function onPlayerTimeUpdate({ duration, percent, seconds }) {
-  // data is an object containing properties specific to that event
-  const playerValue = {
-    duration,
-    percent,
-    seconds,
-  };
-
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(playerValue));
+function onTimeUpdate({ seconds }) {
+  let timeValue = JSON.stringify(seconds);
+  //console.log(timeValue);
+  localStorage.setItem(STORAGE_KEY, timeValue);
 }
 
 savedPlayerValue();
-//перевіряємо чи є дані в LocalStorage
-function savedPlayerValue() {
-  const savedKeyPlayer = localStorage.getItem(STORAGE_KEY);
 
-  if (savedKeyPlayer) {
-    player.setCurrentTime(JSON.parse(savedKeyPlayer));
+function savedPlayerValue() {
+  const savedValue = localStorage.getItem(STORAGE_KEY);
+  if (savedValue) {
+    return (parsedTime = JSON.parse(savedValue));
   }
+  return (parsedTime = 0);
 }
+
+player.setCurrentTime(parsedTime);
